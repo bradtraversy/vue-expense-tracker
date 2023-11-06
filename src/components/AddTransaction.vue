525a9f8f213955cf801da53f26972f5cfbca8803
Brad Traversy <traversymedia@gmail.com>
2023-11-06 15:33:34 -0500

    Form, validation & toast

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -1,17 +1,48 @@
 <template>
   <h3>Add new transaction</h3>
-  <form id="form">
+  <form id="form" @submit.prevent="onSubmit">
     <div class="form-control">
       <label for="text">Text</label>
-      <input type="text" id="text" placeholder="Enter text..." />
+      <input type="text" id="text" placeholder="Enter text..." v-model="text" />
     </div>
     <div class="form-control">
       <label for="amount"
         >Amount <br />
         (negative - expense, positive - income)</label
       >
-      <input type="text" id="amount" placeholder="Enter amount..." />
+      <input
+        type="text"
+        id="amount"
+        placeholder="Enter amount..."
+        v-model="amount"
+      />
     </div>
     <button class="btn">Add transaction</button>
   </form>
 </template>
+
+<script setup>
+import { useToast } from 'vue-toastification';
+import { ref } from 'vue';
+
+const text = ref('');
+const amount = ref('');
+
+// Get toast interface
+const toast = useToast();
+
+const onSubmit = () => {
+  if (!text.value || !amount.value) {
+    // Display a toast error message if either field is empty
+    toast.error('Both fields must be filled.');
+    return;
+  }
+
+  // Proceed with form submission logic here...
+  console.log('Form submitted:', text.value, amount.value);
+
+  // Clear form fields
+  text.value = '';
+  amount.value = '';
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import './assets/style.css';
-
 import { createApp } from 'vue';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
+import './assets/style.css';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+app.use(Toast);
+app.mount('#app');
